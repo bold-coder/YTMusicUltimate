21a67a11845e5459cf9c6bd3b1ed6dd2a28b19d6
bold-coder <kai.gofile@yahoo.com>
2025-09-26 19:53:55 -0400

    LyricsManager.h

---
+++ Source/LyricsManager.h
@@ -0,0 +1,10 @@
+#import <Foundation/Foundation.h>
+
+@interface LyricsManager : NSObject
+
++ (instancetype)sharedInstance;
+- (void)fetchLyricsForSong:(NSString *)songTitle
+                    artist:(NSString *)artistName
+                completion:(void (^)(NSString *lyrics, NSError *error))completion;
+
+@end
