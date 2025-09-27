c62205547ef7ff1188217414f3e45cc7e677a3f2
bold-coder <kai.gofile@yahoo.com>
2025-09-26 20:02:12 -0400

    Delete Source/LyricsViewController.h

--- Source/LyricsViewController.h
+++
@@ -1,9 +0,0 @@
-#import <UIKit/UIKit.h>
-
-@interface LyricsViewController : UIViewController
-
-@property (nonatomic, strong) NSString *lyricsText;
-@property (nonatomic, strong) NSString *songTitle;
-@property (nonatomic, strong) NSString *artistName;
-
-@end
