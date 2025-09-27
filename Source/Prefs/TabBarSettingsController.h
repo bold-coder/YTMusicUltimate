f854000ab139aaf0a3bbb9f6ccae49b5af01dd52
bold-coder <kai.gofile@yahoo.com>
2025-09-26 22:14:58 -0400

    TabBarSettingsController.h

--- Source/Prefs/TabBarSettingsController.h
+++ Source/Prefs/TabBarSettingsController.h
@@ -1,11 +1,6 @@
 #import <UIKit/UIKit.h>
 #import "../Headers/Localization.h"
 
-@interface OtherSettingsController : UIViewController <UITableViewDelegate, UITableViewDataSource>
-@property (nonatomic, strong) UITableView* tableView;
-@end
-
-@interface YTAssetLoader : NSObject
-- (instancetype)initWithBundle:(NSBundle *)bundle;
-- (UIImage *)imageNamed:(NSString *)image;
+@interface TabBarSettingsController : UIViewController <UITableViewDelegate, UITableViewDataSource>
+@property (nonatomic, strong) UITableView *tableView;
 @end
